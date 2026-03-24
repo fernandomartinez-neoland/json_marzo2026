106b119ea29409a89a6c9e92981bf95414ce6d7a
fernandomartinez-neoland <fernando.martinez@neoland.es>
2026-03-24 09:55:39 +0100

    cosas

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const json_pelota = {
     "peso": "500g"
 
 }
-
+// el nombre de las propiedades puede ir o no dentro de comillas
 const usuario = {
     nombre: "fernando",
     "apellido": "martinez",
@@ -24,9 +24,9 @@ const boton={
 // boton.style.backgroundColor
 // variable.propiedad
 console.log(json_pelota)
-console.log(json_pelota.textura)
-console.log(usuario.objeto.propiedades)
-console.log(parseInt(usuario.direcciones[6]))
+console.log(json_pelota.textura)//imprime "rugosa" que es el valor de la variable json_pelota y su propiedad llamada "textura"
+console.log(usuario.objeto.propiedades)//imprime "valor", que es el valor del objeto "usuario", la propiedad que es de tipo objeto llamado "objeto" y su propiedad interna llamada "propiedades"
+console.log(parseInt(usuario.direcciones[6]))//imprime el "10" del objeto usuario
 const arreglo=[]
 arreglo[10]="mario"
 console.log(arreglo)
@@ -35,4 +35,14 @@ json_pelota.nombre="pelotita"
 console.log(json_pelota)
 
 // variable["propiedad"]
-console.log('json_pelota["nombre"]: ', json_pelota["nombre"])
+console.log('json_pelota["nombre"]: ', json_pelota["nombre"]);
+
+
+const nombre="fernando"
+
+const profe={
+    nombre
+}
+// const profe={
+//     nombre:"fernando"
+// }
